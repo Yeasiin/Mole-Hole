16b1c26adee3f104bc69c353531afe98f8917999
Yeasin Arfat <yeasinarfatemon12345@gmail.com>
2022-03-14 21:47:15 +0600

    landing page for game

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+const startBtn = document.querySelector(".start__btn");
+const stopBtn = document.querySelector(".stop__btn");
 const holes = document.querySelectorAll(".hole");
+const gameEndIn = document.querySelector(".gameEndIn");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
-let lastHole, peepTime;
-let timeup = false;
+
+let lastHole, peepTime, timer;
+let timeUp = false;
+let gameTime = 30000;
+let gameTimer = gameTime / 1000;
 let score = 0;
+
+gameEndIn.textContent = gameTimer + " Seconds";
 
 function randTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -19,31 +27,55 @@ function randHoles(holes) {
   return hole;
 }
 
-
-function peep(){
-    const time = randTime(200, 800);
-    const hole = randHoles(holes);
-    hole.classList.add("up");
-    peepTime = setTimeout(() => {
-        hole.classList.remove("up");
-        if(!timeup) peep();
-    }, time);
+function peep() {
+  const time = randTime(300, 1000);
+  // const time = randTime(100000, 220000);
+  const hole = randHoles(holes);
+  hole.classList.add("up");
+  peepTime = setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) peep();
+  }, time);
 }
 
-function startGame(){
-    clearTimeout(peepTime)
-    timeup = false;
-    score = 0;
-    peep();
-    setTimeout(() => {
-        timeup = true;
-    }, 60000);
-}
-function bonk (e){
-    if(!e.isTrusted) return;
-    score++;
-    this.parentElement.classList.remove("up");
-    scoreBoard.textContent = score;
+function startGame() {
+  clearInterval(timer);
+  clearTimeout(peepTime);
+  timeUp = false;
+  score = 0;
+  gameTimer = gameTime / 1000;
+  gameEndIn.textContent = gameTimer + " Seconds";
+  scoreBoard.textContent = score;
+  peep();
+  setTimeout(() => {
+    timeUp = true;
+  }, gameTime);
+
+  timer = setInterval(() => {
+    updateSec();
+  }, 1000);
 }
 
-moles.forEach(mole => mole.addEventListener("click", bonk));
+function bonk(e) {
+  if (!e.isTrusted) return;
+  score++;
+  this.parentElement.style.backgroundImage = "url(bang.png)";
+  this.style.zIndex = "-1";
+  setTimeout(() => {
+    this.style.zIndex = "";
+    this.parentElement.style.background = "";
+  }, 500);
+
+  scoreBoard.textContent = score;
+}
+
+moles.forEach((mole) => mole.addEventListener("click", bonk));
+
+function updateSec() {
+  gameEndIn.textContent = --gameTimer + " Seconds";
+
+  if (gameTimer < 0) {
+    gameEndIn.textContent = "Game Over";
+    clearInterval(timer);
+  }
+}
